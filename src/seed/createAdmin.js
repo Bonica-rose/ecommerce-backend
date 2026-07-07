0aafaa5c6f18deff8ed3606120bcc2b8b6285da5
Bonica-rose <eveningstarforever@gmail.com>
2026-07-07 22:06:24 +0530

    seeder run file path modified

--- src/seed/createAdmin.js
+++ src/seed/createAdmin.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+const User = require("../models/user.model");
 
 mongoose.connect(process.env.MONGODB_URI);
 
